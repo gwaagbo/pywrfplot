1f7551f51d44435b933dad604748e1c354dd0d26
gwaagbo@gmail.com <gwaagbo@gmail.com@9498168d-04fa-24f1-478f-5dcbd8e16d8b>
2012-05-11 13:30:30 +0000

    Expanding example

--- example.py
+++ example.py
@@ -7,7 +7,9 @@
 import mapWRF
 import xzWRF
 import tzWRF
+import skewT
 
 mapWRF.mapDomains(includePMSL=True)
 xzWRF.xzCloudPlot(nest=4, time=45, plotTemp=True, plotRH=False)
+skewT.skewTPlot(nest=4, time=45)
 tzWRF.tzCloudPlot(nest=4)
